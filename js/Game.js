23bf3c97b4f8ace292240628fd8ecdfbca8e6d40
smarquina37 <56809615+smarquina37@users.noreply.github.com>
2022-09-22 12:18:42 -0700

    Added Game and Phrase classes

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,39 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
+class Game {
+  constructor() {
+    this.missed = 0;
+    this.phrases = [
+      {phrase: 'work hard dream big'},
+      {phrase: 'success looks good on you'},
+      {phrase: 'you can do hard things'},
+      {phrase: 'no pressure no diamonds'},
+      {phrase: 'Tap into your magic'}
+  ];
+    this.activePhrase = null;
+  }
+
+  /*
+  * Starts game
+  */
+  // startGame() {
+  //   getRandomPhrase();
+  //   this.activePhrase.addPhraseToDisplay();
+  // }
+
+  /** 
+  * Randomly retrieves a phrase from phrases property
+  * @return {Object} Phrase object chosen to be used
+  */
+
+  getRandomPhrase() {
+    let randomIndex = Math.floor(Math.random() * this.phrases.length);
+      return this.phrases[randomIndex];
+  }
+
+  /*
+  * Checks to see if button clicked by the player matches letter in the phrase
+  */
+//  handleInteraction() {
+
+//  }
+  
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,18 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Phrase.js */
+class Phrase {
+  constructor(phrase) {
+    this.phrase = phrase.toLowerCase();
+  }
+
+/**
+* Display phrase on game board
+*/
+  addPhraseToDisplay() {
+    
+  }
+  // checkLetter() {
+
+  // }
+  // showMatchedLetter() {
+
+  // }
+}
